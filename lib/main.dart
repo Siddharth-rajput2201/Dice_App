3ae956a73cba6a42c13d3d677def640720e14752
Hush <rajputsiddharth18@gmail.com>
2020-07-20 00:45:09 +0530

    added images

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      backgroundColor: Colors.red,
-      appBar: AppBar(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
         backgroundColor: Colors.red,
-        title: Text('Dice'),
-      ),
-      body: DiceApp(),
-    )
-  ));
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text('Dice'),
+        ),
+        body: DiceApp(),
+      )));
 }
 
 class DiceApp extends StatelessWidget {
+
+  int leftdicenumber = 3;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Center(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset("assets/images/dice$leftdicenumber.png"),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset("assets/images/dice$leftdicenumber.png"),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          OutlineButton(onPressed: (){} , child:  Text("PRESS",style: TextStyle(fontSize: 20),),
+            borderSide: BorderSide(color: Colors.white,width: 2),
+            color: Colors.white,
+            textColor: Colors.white,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),)
+        ],
+      ),
+    );
   }
 }
-
-
